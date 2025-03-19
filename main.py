713a2a50775d23289333951db4ec1f777b66354b
joyboy125 <mojawatjay26@gmail.com>
2025-03-19 22:32:12 +0530

    sf

--- main.py
+++ main.py
@@ -33,15 +33,10 @@ plugins={"root": "plugins"}
 
 animebot = Client(name="animemonster_bot", api_id=api_id, api_hash=api_hash, bot_token=animemonster_bot, plugins=plugins, sleep_threshold=15)
 
-Joyboy2bot = Client(name="Joyboy2_bot", api_id=api_id, api_hash=api_hash, bot_token=Joyboy2_bot, plugins=plugins, sleep_threshold=15)
-
-manhwabot = Client(name="manhwa_robot", api_id=api_id, api_hash=api_hash, bot_token=manhwa_robot, plugins=plugins, sleep_threshold=15)
 
 async def main():
     try:
         await animebot.start()
-        await Joyboy2bot.start()
-        await manhwabot.start()
         print("Bots are running...")
         await idle()
     except (ApiIdInvalid, ApiIdPublishedFlood):
@@ -50,63 +45,9 @@ async def main():
         raise Exception("Your BOT_TOKEN is not valid.")
     finally:
         await animebot.stop()
-        await Joyboy2bot.stop()
-        await manhwabot.stop()
         print("Bot stopped.")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(main())
     print("Bot stopped.")
-
-
-# class animemonster(Client):
-#     def __init__(self):
-#         super().__init__(
-#             name="animemonster_bot",
-#             api_id=api_id,
-#             api_hash=api_hash,
-#             bot_token=animemonster_bot,
-#             workers=200,
-#             plugins={"root": "plugins"},
-#             sleep_threshold=15,
-#         )
-
-# class Joyboy2(Client):
-#     def __init__(self):
-#         super().__init__(
-#             name="Joyboy2_bot",
-#             api_id=api_id,
-#             api_hash=api_hash,
-#             bot_token=Joyboy2_bot,
-#             workers=200,
-#             plugins={"root": "plugins"},
-#             sleep_threshold=15,
-#         )
-
-# class manhwa(Client):
-#     def __init__(self):
-#         super().__init__(
-#             name="manhwa_robot",
-#             api_id=api_id,
-#             api_hash=api_hash,
-#             bot_token=manhwa_robot,
-#             workers=200,
-#             plugins={"root": "plugins"},
-#             sleep_threshold=15,
-#         )
-
-
-# async def main():
-
-#     bot1 = animemonster()
-#     bot2 = Joyboy2()
-#     bot3 = manhwa()
-
-#     await asyncio.gather(bot1.start(), bot2.start(), bot3.start())
-#     print("Both bots are running...")
-
-#     await asyncio.Event().wait()
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
